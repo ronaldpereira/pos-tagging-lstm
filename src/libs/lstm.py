127bbea67a322d9632f175748185700114cff129
Ronald <ronald.drp11@gmail.com>
2018-11-10 12:16:05 -0200

    removed custom accuracy function

--- src/libs/lstm.py
+++ src/libs/lstm.py
@@ -14,21 +14,9 @@ class Model:
         self.model.add(TimeDistributed(Dense(len(data.tag2index))))
         self.model.add(Activation('softmax'))
 
-        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy', Model.ignore_padding_accuracy(0)])
+        self.model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 
         self.model.summary()
-
-    @staticmethod
-    def ignore_padding_accuracy(ignored_tag_id=0):
-        def ignore_accuracy(y_true, y_pred):
-            y_true_class = K.argmax(y_true, axis=-1)
-            y_pred_class = K.argmax(y_pred, axis=-1)
-    
-            ignore_mask = K.cast(K.not_equal(y_pred_class, ignored_tag_id), 'int32')
-            matches = K.cast(K.equal(y_true_class, y_pred_class), 'int32') * ignore_mask
-            accuracy = K.sum(matches) / K.maximum(K.sum(ignore_mask), 1)
-            return accuracy
-        return ignore_accuracy
 
     def train(self, x_train, y_train):        
         self.model.fit(x_train, y_train, batch_size=128, epochs=40, validation_split=0.2)
